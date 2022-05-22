718cfd77a007acbbf57d576eba02b8a991d10fc0
Faizan Ejaz <abc@example.com>
2022-05-22 17:52:12 +0500

    All Assests and Images added

--- app/src/main/java/com/example/bsef19a028_assignment2/MainActivity.java
+++ app/src/main/java/com/example/bsef19a028_assignment2/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.bsef19a028_assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        b1= findViewById(R.id.button5);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , Activity2.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
