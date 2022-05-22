d6dc2aa2973018e9de5f57b4be60e7be9a049d98
Faizan Ejaz <abc@example.com>
2022-05-22 18:20:01 +0500

    Second Screen + Intent Done

---
+++ app/src/main/java/com/example/bsef19a028_assignment2/Activity__3.java
@@ -0,0 +1,14 @@
+package com.example.bsef19a028_assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity__3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__3);
+    }
+}
